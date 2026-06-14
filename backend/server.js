7dffc0d262f69a60a6f61c4d6b9ec9befc3195f7
manju0389 <manjunathrao311989@gmail.com>
2026-06-14 17:37:12 +0530

    update project

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ app.use(
 cors({
 origin: [
 "http://localhost:5173",
-"https://sai-angels-frontend.onrender.com"
+"https://sai-angels-college-ysdr.onrender.com"
 ],
 credentials: true,
 methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
