76afbfa246df8068b4df43dedea2b454211d2a13
manju0389 <manjunathrao311989@gmail.com>
2026-06-17 18:09:47 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,10 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGODB_URI);
+
     console.log("✅ MongoDB Connected");
+    console.log("📦 DB Name:", mongoose.connection.name);
+
   } catch (error) {
     console.error("❌ MongoDB Connection Error:", error.message);
     process.exit(1);
